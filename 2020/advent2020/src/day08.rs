aa0930edba3d2c055609693e1b7d4d13f941a968
Gweneth Malmquist <gmalmquist@squareup.com>
2020-12-09 20:42:25 -0500

    Done with day 8.

--- 2020/advent2020/src/day08.rs
+++ 2020/advent2020/src/day08.rs
@@ -25,43 +25,72 @@ pub fn main() {
       let lines = lines.filter(|l| l.is_ok())
           .map(|l| l.unwrap());
 
-      let program: Vec<Instruction> = lines.map(|line| Instruction::parse(&line))
+      let mut program: Vec<Instruction> = lines.map(|line| Instruction::parse(&line))
           .collect();
       println!("Loaded {} line program.", program.len());
 
-      let mut executed: HashSet<usize> = HashSet::new();
-      let mut fp = 0;
-      let mut acc = 0;
+      println!("Unmodified program: {:?}", execute(&program));
 
-      while fp < program.len() {
-          if executed.contains(&fp) {
-              println!("Halting before loop, acc = {}", acc);
+      for i in 0..program.len() {
+          let inst = &program[i];
+          if inst.cmd == "acc" {
+              continue;
+          }
+          let og = inst.clone();
+
+          let mut inst = inst.clone();
+          inst.cmd = (if &og.cmd == "jmp" { "nop" } else { "jmp" }).to_string();
+          program[i] = inst.clone();
+          if let ExecutionResult::Halt(acc) = execute(&program) {
+              println!("Halted by flipping #{} to {} with acc: {}",
+                       i, inst.cmd, acc);
               break;
           }
-          executed.insert(fp);
-          let Instruction { cmd, amt } = &program[fp];
-          let cmd: &str = &cmd;
-          match (cmd, amt) {
-              ("nop", _) => {
-                  fp += 1;
-              },
-              ("acc", amt) => {
-                  acc += amt;
-                  fp += 1;
-              },
-              ("jmp", amt) => {
-                  fp = (fp as i32 + *amt) as usize;
-              },
-              unrecognized => {
-                  println!("Unrecognized instruction: {:?}", unrecognized);
-                  break;
-              },
-          }
+          program[i] = og;
       }
   }
 }
 
+fn execute(program: &Vec<Instruction>) -> ExecutionResult {
+  let mut executed: HashSet<usize> = HashSet::new();
+  let mut fp = 0;
+  let mut acc: i32 = 0;
+
+  while fp < program.len() {
+      if executed.contains(&fp) {
+          return ExecutionResult::Loop(acc);
+      }
+      executed.insert(fp);
+      let Instruction { cmd, amt } = &program[fp];
+      let cmd: &str = &cmd;
+      match (cmd, amt) {
+          ("nop", _) => {
+              fp += 1;
+          },
+          ("acc", amt) => {
+              acc += amt;
+              fp += 1;
+          },
+          ("jmp", amt) => {
+              fp = (fp as i32 + *amt) as usize;
+          },
+          unrecognized => {
+              println!("Unrecognized instruction: {:?}", unrecognized);
+              panic!("Bad instruction.");
+          },
+      }
+  }
+
+  ExecutionResult::Halt(acc)
+}
+
 #[derive(Debug)]
+enum ExecutionResult {
+    Halt(i32),
+    Loop(i32),
+}
+
+#[derive(Clone, Debug)]
 struct Instruction {
     cmd: String,
     amt: i32,
